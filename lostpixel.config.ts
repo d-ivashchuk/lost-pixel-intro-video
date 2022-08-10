bad22b491867120086c402322ba167234a107fa1
d-ivashchuk <dimaiv@me.com>
2022-08-10 11:55:29 +0200

    add lost pixel

---
+++ lostpixel.config.ts
@@ -0,0 +1,18 @@
+import { CustomProjectConfig } from "lost-pixel";
+
+export const config: CustomProjectConfig = {
+  storybookShots: {
+    storybookUrl: "./storybook-static",
+  },
+  pageShots: {
+    pages: [
+      { path: "/a", name: "a", id: "a" },
+      { path: "/b", name: "b", id: "b" },
+      { path: "/c", name: "c", id: "c" },
+      { path: "/d", name: "d", id: "d" },
+    ],
+    pageUrl: "http://localhost:3000",
+  },
+  generateOnly: true,
+  failOnDifference: true,
+};
